ec9d014bdb834adaf228ea030c08f6ef45131e63
Anh Nguyen <anh.nguyen@playnext-lab.co.jp>
2020-03-30 09:20:36 +0900

    Add connection and room channel for cable

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,26 @@
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    @room = Room.includes(:users, :messages).find_by(id: params[:room])
+    stream_for @room
+  end
+
+  def receive(data)
+    SaveMessageWorker.perform_async({
+      room_id: @room.id,
+      sender_id: current_user.id,
+      sent_at: DateTime.current,
+      content: data["message"]
+    })
+    RoomChannel.broadcast_to(@room, { room: @room, users: @room.users, messages: data["message"] })
+  end
+
+  def unsubscribed
+  end
+
+private
+
+  def message_params
+    params.permit(:sender_id, :sent_at, :content)
+    params[:room_id] = @room.id
+  end
+end
