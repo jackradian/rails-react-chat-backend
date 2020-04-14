2a2cd2615ee2c0b99d5cd3cf52cc8f35251ff281
Anh Nguyen <anh.nguyen@playnext-lab.co.jp>
2020-04-14 10:53:28 +0900

    Add rbenv prefix to puma command

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ set :keep_releases, 3
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.7.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl puma pumactl}
 set :rbenv_roles, :all # default value
 
 # nginx
