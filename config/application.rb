57424505a0e6de33a696ac083434d91b6b6925b6
Anh Nguyen <anh.nguyen@playnext-lab.co.jp>
2020-04-14 23:39:37 +0900

    Config rails running at subfolder

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Rails6ChatApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.relative_url_root = ENV['RAILS_RELATIVE_URL_ROOT']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
