5f70fec631eb222a09f3a21e53e026fc6ec141b6
Anh Nguyen <anhnt2808@gmail.com>
2022-10-20 00:42:22 +0900

    Add route specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'users#index'
+  # root to: 'users#index'
 
   post "signup", to: "users#create", as: "signup"
   post "login", to: "sessions#create", as: "login"
   get "logout", to: "sessions#destroy", as: "logout"
 
-  get "get_friends", to: "users#all_friends", as: "get_friends"
+  # get "get_friends", to: "users#all_friends", as: "get_friends"
   post "add_friend", to: "users#add_friend", as: "add_friend"
   get "my_direct_rooms", to: "rooms#my_direct_rooms", as: "my_direct_rooms"
 end
---
+++ spec/routing/rooms_routing_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe RoomsController, type: :routing do
+  describe "routing" do
+    it do
+      should route(:get, '/my_direct_rooms').
+        to(controller: :rooms, action: :my_direct_rooms)
+    end
+  end
+end
